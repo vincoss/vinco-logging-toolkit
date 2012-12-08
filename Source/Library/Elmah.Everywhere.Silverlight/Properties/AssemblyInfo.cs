493d545e5d36670572e36a223d34074b13b56773
Ferdinand Lukasak <feroalien@hotmail.com>
2012-12-08 22:05:56 +1100

    Updated NuGet package

--- Source/Library/Elmah.Everywhere.Silverlight/Properties/AssemblyInfo.cs
+++ Source/Library/Elmah.Everywhere.Silverlight/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.2.1946.197")]
-[assembly: AssemblyFileVersion("1.2.1946.197")]
+[assembly: AssemblyVersion("1.2.2154.575")]
+[assembly: AssemblyFileVersion("1.2.2154.575")]
--- Source/Library/Elmah.Everywhere/Properties/AssemblyInfo.cs
+++ Source/Library/Elmah.Everywhere/Properties/AssemblyInfo.cs
@@ -33,7 +33,7 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.2.2035.573")]
-[assembly: AssemblyVersion("1.2.2035.573")]
-[assembly: AssemblyFileVersion("1.2.2035.573")]
+// [assembly: AssemblyVersion("1.2.2151.576")]
+[assembly: AssemblyVersion("1.2.2151.576")]
+[assembly: AssemblyFileVersion("1.2.2151.576")]
 
