ce0f5855b2db523f9e750495675bde722667c32c
Ferdinand Lukasak <feroalien@hotmail.com>
2013-04-13 20:21:32 +1000

    Updated nuget package

--- Source/Library/Elmah.Everywhere/Properties/AssemblyInfo.cs
+++ Source/Library/Elmah.Everywhere/Properties/AssemblyInfo.cs
@@ -33,7 +33,7 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.2.1936.590")]
-[assembly: AssemblyVersion("1.2.1936.590")]
-[assembly: AssemblyFileVersion("1.2.1936.590")]
+// [assembly: AssemblyVersion("1.2.1957.592")]
+[assembly: AssemblyVersion("1.2.2901.597")]
+[assembly: AssemblyFileVersion("1.2.2901.597")]
 
