bf0d38220092e21f4459478dca046ad899f5982e
Ferdinand Lukasak <feroalien@hotmail.com>
2014-06-09 20:29:45 +1000

    updated assembly version

--- Source/Library/Elmah.Everywhere.WP8/Properties/AssemblyInfo.cs
+++ Source/Library/Elmah.Everywhere.WP8/Properties/AssemblyInfo.cs
@@ -9,9 +9,9 @@ using System.Resources;
 [assembly: AssemblyTitle("Elmah.Everywhere.WP8")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Vincoss")]
 [assembly: AssemblyProduct("Elmah.Everywhere.WP8")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyCopyright("Copyright © Vincoss 2011")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,6 +32,4 @@ using System.Resources;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: NeutralResourcesLanguageAttribute("en-US")]
+[assembly: AssemblyVersion("1.2.3000.*")]
--- Source/Library/Elmah.Everywhere/Properties/AssemblyInfo.cs
+++ Source/Library/Elmah.Everywhere/Properties/AssemblyInfo.cs
@@ -34,6 +34,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.2.1957.592")]
-[assembly: AssemblyVersion("1.2.2901.601")]
-[assembly: AssemblyFileVersion("1.2.2901.601")]
+[assembly: AssemblyVersion("1.2.3000.*")]
 
