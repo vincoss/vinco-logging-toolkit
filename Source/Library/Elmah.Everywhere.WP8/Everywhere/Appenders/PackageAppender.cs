cfdbf6345bc39bde11c2fadc887e7bf8ea312d0a
Ferdinand Lukasak <feroalien@hotmail.com>
2014-06-09 18:38:50 +1000

    Added item order and updated message

--- Source/Library/Elmah.Everywhere.WP8/Everywhere/Appenders/PackageAppender.cs
+++ Source/Library/Elmah.Everywhere.WP8/Everywhere/Appenders/PackageAppender.cs
@@ -13,21 +13,26 @@ namespace Elmah.Everywhere.Appenders
             var sb = new StringBuilder();
             var location = Package.Current.InstalledLocation;
 
-            sb.AppendLine(string.Format("Name:          {0}", location.Name));
-            sb.AppendLine(string.Format("DateCreated:   {0}", location.DateCreated));
-            sb.AppendLine(string.Format("PackagePath:   {0}", location.Path));
+            sb.AppendLine(string.Format("InstallFolderName:     {0}", location.Name));
+            sb.AppendLine(string.Format("DateCreated:           {0}", location.DateCreated));
+            sb.AppendLine(string.Format("InstallPackagePath:    {0}", location.Path));
             sb.AppendLine();
 
             var folder = Package.Current.InstalledLocation;
             foreach (var file in await folder.GetFilesAsync())
             {
-                sb.AppendLine(string.Format("Name:          {0}", file.Name));
-                sb.AppendLine(string.Format("DateCreated:   {0}", file.DateCreated));
-                sb.AppendLine(string.Format("Path:          {0}", file.Path));
+                sb.AppendLine(string.Format("Name:                  {0}", file.Name));
+                sb.AppendLine(string.Format("DateCreated:           {0}", file.DateCreated));
+                sb.AppendLine(string.Format("Path:                  {0}", file.Path));
                 sb.AppendLine();
             }
 
-            errorInfo.AddDetail(this.Name, "Files", sb.ToString());
+            errorInfo.AddDetail(this.Name, "", sb.ToString());
+        }
+
+        public override int Order
+        {
+            get { return 3; }
         }
 
         public override string Name
