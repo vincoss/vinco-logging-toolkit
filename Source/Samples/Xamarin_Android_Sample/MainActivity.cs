9e87c746f2016e3aabd5ac744fe6c5566729e8f2
Ferdinand Lukasak <feroalien@hotmail.com>
2014-07-10 16:26:17 +1000

    Added Android sample project

---
+++ Source/Samples/Xamarin_Android_Sample/MainActivity.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+
+namespace Xamarin_Android_Sample
+{
+    [Activity(Label = "Xamarin_Android_Sample", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+        }
+    }
+}
